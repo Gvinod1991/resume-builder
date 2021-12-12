1a206dde1818aa16a5a306dc8a6ccdaadcc4fa45
Godti Vinod <venkat.godti3@gmail.com>
2021-12-12 13:45:46 +0530

    Build issue fix

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -47,8 +47,8 @@ export const Layout: React.FunctionComponent<SideBarProps> = ({ children }: Side
                   </div>
                   {menuOpen ?
                     <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button">
-                      <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" id="user-menu-item-1">Settings</a>
-                      <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" id="user-menu-item-2">Sign out</a>
+                      <a href="/" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" id="user-menu-item-1">Settings</a>
+                      <a href="/" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" id="user-menu-item-2">Sign out</a>
                     </div>
                     : null
                   }
