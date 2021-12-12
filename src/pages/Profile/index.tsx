5e258b424f0656a6178ecc18a4615caeb6aef1b4
Godti Vinod <venkat.godti3@gmail.com>
2021-12-12 13:37:14 +0530

    Profile and Resume pages creation

---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+export default function Profile() {
+  return (
+    <React.Fragment>
+      <p>Profile page</p>
+    </React.Fragment>
+  )
+}
---
+++ src/pages/Resume/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Resume() {
+  return (
+    <React.Fragment>
+      <p>Resume page</p>
+    </React.Fragment>
+  )
+}
