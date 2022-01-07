5e74875c141374c95b6f36f42653345cbf1d1f6e
Godti Vinod <venkat.godti3@gmail.com>
2022-01-07 21:39:33 +0530

    Card component usage

---
+++ src/Pages/Profile/About/index.tsx
@@ -0,0 +1,10 @@
+import { Input } from '../../../Components';
+
+export const About = () => {
+  return (
+    <>
+      About
+      <Input inputLabel='Your Location' handleChange={() => { }} />
+    </>
+  )
+}
--- src/Pages/Profile/index.tsx
+++ src/Pages/Profile/index.tsx
@@ -1,29 +1,41 @@
 import React from 'react';
-import { Button } from '../../Components';
 import { Tabs, TabPane } from '../../Components';
-
+import { About } from './About';
+import { Card } from '../../Components';
 
 export default function Profile() {
   return (
     <Tabs className="sm:block sm:ml-6">
       <>
         <TabPane tab={"OverView"} key={1}>
-          OverView
+          <Card>
+            Overview
+          </Card>
         </TabPane>
         <TabPane tab={"About"} key={2}>
-          About
+          <Card>
+            <About />
+          </Card>
         </TabPane>
         <TabPane tab={"Side Projects"} key={3}>
-          Side Projects
+          <Card>
+            Side Projects
+          </Card>
         </TabPane>
         <TabPane tab={"Skills"} key={4}>
-          Skills
+          <Card>
+            Skills
+          </Card>
         </TabPane>
         <TabPane tab={"Achievements"} key={5}>
-          Achievements
+          <Card>
+            Achievements
+          </Card>
         </TabPane>
         <TabPane tab={"Education"} key={6}>
-          Education
+          <Card>
+            Education
+          </Card>
         </TabPane>
       </>
     </Tabs>
