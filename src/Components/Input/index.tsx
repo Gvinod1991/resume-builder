42ba095d4d218a0c8ba6002da67b09ef05fc1f32
Godti Vinod <venkat.godti3@gmail.com>
2022-01-08 15:20:32 +0530

    Input component update

--- src/Components/Input/index.tsx
+++ src/Components/Input/index.tsx
@@ -1,15 +1,21 @@
 
 interface inputProps {
   inputLabel: string,
+  value?: string,
+  readonly?: boolean,
+  disabled?: boolean,
   handleChange: () => void
 }
 
-export const Input = ({ inputLabel, handleChange }: inputProps) => {
-  return <div className="flex flex-col">
+export const Input = ({ inputLabel, value, handleChange, readonly, disabled }: inputProps) => {
+  return <div className="flex flex-col p-1">
     <label>{inputLabel}</label>
     <input
       type="text"
-      className="mt-1 block w-full rounded-md bg-gray-100 focus:border-indigo-600 border-2 focus:bg-white focus:ring-0"
+      readOnly={readonly}
+      value={value || ""}
+      disabled={disabled}
+      className="w-full rounded-md shadow-sm bg-gray-100 focus:border-indigo-500 focus:bg-white focus:ring-indigo-500"
       placeholder={inputLabel}
       onChange={handleChange}
     />
