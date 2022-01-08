146ac0bc92f700805e3715cc90a0c040f91a4926
Godti Vinod <venkat.godti3@gmail.com>
2022-01-08 15:21:20 +0530

    Textarea component update

--- src/Components/TextArea/index.tsx
+++ src/Components/TextArea/index.tsx
@@ -10,7 +10,7 @@ export const TextArea = ({ textAreaValue, textAreLabel, handleChange }: TextArea
     <div className="flex flex-col p-1">
       <label>{textAreLabel}</label>
       <textarea
-        className='mt-1 block w-full rounded-md bg-gray-100 focus:border-indigo-600 border-2 focus:bg-white focus:ring-0'
+        className='w-full rounded-md shadow-sm bg-gray-100 focus:border-indigo-500 focus:bg-white focus:ring-indigo-500'
         onChange={handleChange}
         value={textAreaValue}
       />
