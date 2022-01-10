93ef420ff572da43d57b2e83f02ce88aafcf8e79
Godti Vinod <venkat.godti3@gmail.com>
2022-01-10 14:51:09 +0530

    Input component type interface update

--- src/Components/Input/index.tsx
+++ src/Components/Input/index.tsx
@@ -1,23 +1,17 @@
+import { InputHTMLAttributes } from 'react';
 
-interface inputProps {
-  inputLabel: string,
-  value?: string,
-  readonly?: boolean,
-  disabled?: boolean,
-  handleChange: () => void
+interface inputProps extends InputHTMLAttributes<HTMLInputElement> {
+  inputLabel?: string,
+  name?: string
 }
 
-export const Input = ({ inputLabel, value, handleChange, readonly, disabled }: inputProps) => {
+export const Input = ({ inputLabel, name, ...rest }: inputProps) => {
   return <div className="flex flex-col p-1">
     <label>{inputLabel}</label>
     <input
       type="text"
-      readOnly={readonly}
-      value={value || ""}
-      disabled={disabled}
       className="w-full rounded-md shadow-sm bg-gray-100 focus:border-indigo-500 focus:bg-white focus:ring-indigo-500"
-      placeholder={inputLabel}
-      onChange={handleChange}
+      {...rest}
     />
   </div>
 }
