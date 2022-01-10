756b1023854fbf6979b05b53c31379a169be4fae
Godti Vinod <venkat.godti3@gmail.com>
2022-01-10 23:19:04 +0530

    Extend ButtonHTMLAttributes from ButtonType interface for better button component props type

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -1,11 +1,22 @@
-interface ButtonType {
+import { ReactNode, ButtonHTMLAttributes } from 'react';
+
+interface ButtonType extends ButtonHTMLAttributes<HTMLButtonElement> {
   title: string;
   className?: string;
-  onClick?: () => void
+  leftIcon?: ReactNode,
+  rightIcon?: ReactNode
 }
 
-export const Button = ({ title, onClick, className }: ButtonType) => {
+export const Button = ({ title, onClick, className, leftIcon, rightIcon }: ButtonType) => {
   return (
-    <button className={`${className} bg-indigo-700 text-white p-2 rounded-md border-0 hover:bg-indigo-800 focus:outline-none`} onClick={onClick}>{title}</button>
+    <button className={`${className} bg-indigo-700 text-white p-2 rounded-md border-0 hover:bg-indigo-800 focus:outline-none`} onClick={onClick}>
+      <span className="inline-block align-sub">
+        {leftIcon}
+      </span>
+      {title}
+      <span className="inline-block align-sub">
+        {rightIcon}
+      </span>
+    </button>
   )
 }
