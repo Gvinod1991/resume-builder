1a571b2cf2b5fb82bd4bc40fe4cb2c94b196e804
Godti Vinod <venkat.godti3@gmail.com>
2022-01-11 20:29:54 +0530

    Modal compoment use in add experience

--- src/Pages/Profile/WorkExperience/index.tsx
+++ src/Pages/Profile/WorkExperience/index.tsx
@@ -1,15 +1,64 @@
-import { Card, Wrapper, Typography, Divider, Button } from "../../../Components";
+import {
+  Card, Wrapper, Typography,
+  Divider, Button, Input, Select, TextArea
+} from "../../../Components";
 import { PlusIcon } from "@heroicons/react/solid";
-
+import { Modal } from "../../../Components";
+import { useState } from "react";
+const options = [{
+  optionKey: '1',
+  optionValue: 'FrontEnd Engineer'
+}, {
+  optionKey: '2',
+  optionValue: 'Backend Engineer'
+}
+]
+const yearOptions = [{
+  optionKey: '1',
+  optionValue: '<1 Year'
+}, {
+  optionKey: '2',
+  optionValue: '1 Year'
+},
+{
+  optionKey: '3',
+  optionValue: '2 Years'
+},
+{
+  optionKey: '4',
+  optionValue: '3 Years'
+}
+]
 export const WorkExperience = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
     <Wrapper className="flex flex-row w-full">
       <>
         <Typography variant="h2" className="w-3/12 text-lg text-gray-600">Your work experience</Typography>
         <Wrapper className='flex flex-col ml-2 px-4 w-8/12'>
           <>
-            <Wrapper className='flex flex-row justify-end'>
-              <Button leftIcon={<PlusIcon className="h-5" />} className="w-fit" title="Add work experience" />
+            <Wrapper className='flex flex-row justify-end z-0'>
+              <>
+                <Button onClick={() => setIsOpen(true)} leftIcon={<PlusIcon className="h-5" />} className="w-fit" title="Add work experience" />
+                <Modal title="Add Work Experience" className="w-6/12" open={isOpen} onClose={() => setIsOpen(false)} >
+                  <>
+                    <Wrapper className='p-2'>
+                      <Input inputLabel='Your Location' />
+                    </Wrapper>
+                    <Wrapper className='p-2'>
+                      <Select
+                        label='Select Designation/Role' handleChange={() => { }} options={options} />
+                    </Wrapper>
+                    <Wrapper className='p-2'>
+                      <Select
+                        label='Years of Experience' handleChange={() => { }} options={yearOptions} />
+                    </Wrapper>
+                    <Wrapper className='p-2'>
+                      <TextArea textAreaValue='' textAreLabel='Professional Intro' handleChange={() => { }} />
+                    </Wrapper>
+                  </>
+                </Modal>
+              </>
             </Wrapper>
             <Card>
               <Wrapper className="flex flex-col w-full">
