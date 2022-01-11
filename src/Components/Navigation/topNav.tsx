4467aab7e0e716fae9c49bd5ad8b191b125a3032
Godti Vinod <venkat.godti3@gmail.com>
2022-01-11 20:30:51 +0530

    ZIndex fix in the dropdown of profile icon in topNav component

--- src/Components/Navigation/topNav.tsx
+++ src/Components/Navigation/topNav.tsx
@@ -15,7 +15,7 @@ export const TopNav = () => {
               <UserCircleIcon className="h-8 w-8 rounded-full" />
             </button>
             {menuOpen ?
-              <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button">
+              <div className="origin-top-right absolute z-10 right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button">
                 <Link to="/settings" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" id="user-menu-item-1">Settings</Link>
                 <Link to="/sign-out" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" id="user-menu-item-2">Sign out</Link>
               </div>
