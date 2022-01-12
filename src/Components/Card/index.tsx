89ceb28ef9f087e52b305bf36f659b1d4079b46c
Godti Vinod <venkat.godti3@gmail.com>
2022-01-12 23:05:09 +0530

    Card component className prop implementation

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -2,11 +2,12 @@ import { ReactChild, ReactChildren } from 'react';
 
 interface CardProps {
   children: ReactChild | ReactChildren
+  className?: string
 }
 
-export const Card = ({ children }: CardProps) => {
+export const Card = ({ children, className }: CardProps) => {
   return <div
-    className="p-6 mx-auto bg-white rounded-md shadow-sm border-gray-200 border-2 flex">
+    className={`p-6 mx-auto bg-white rounded-md shadow-sm border-gray-200 border-2 flex ${className}`}>
     {children}
   </div>
 }
