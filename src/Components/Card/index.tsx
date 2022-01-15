3eb20a5e9dc8fb4444353c70c16747838c23bdc0
Godti Vinod <venkat.godti3@gmail.com>
2022-01-15 20:46:26 +0530

    Card component title implementation

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -2,12 +2,19 @@ import { ReactChild, ReactChildren } from 'react';
 
 interface CardProps {
   children: ReactChild | ReactChildren
-  className?: string
+  className?: string,
+  title?: string
 }
 
-export const Card = ({ children, className }: CardProps) => {
+export const Card = ({ children, className, title }: CardProps) => {
   return <div
-    className={`p-6 mx-auto bg-white rounded-md shadow-sm border-gray-200 border-2 flex ${className}`}>
-    {children}
+    className={`mx-auto bg-white rounded-md shadow-sm border-gray-200 border-2 ${className}`}>
+    {title && <div className='border p-2 flex'>
+      <h1 className='text-xl'>{title}</h1>
+    </div>
+    }
+    <div className='p-4 w-full'>
+      {children}
+    </div>
   </div>
 }
