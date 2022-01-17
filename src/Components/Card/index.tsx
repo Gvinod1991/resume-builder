3b89566936ae1b856fc589ffe30926f20752973f
Godti Vinod <venkat.godti3@gmail.com>
2022-01-17 20:43:50 +0530

    Title class name implementation in card component

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -3,13 +3,14 @@ import { ReactChild, ReactChildren } from 'react';
 interface CardProps {
   children: ReactChild | ReactChildren
   className?: string,
-  title?: string
+  title?: string,
+  titleClassName?: string
 }
 
-export const Card = ({ children, className, title }: CardProps) => {
+export const Card = ({ children, className, title, titleClassName }: CardProps) => {
   return <div
     className={`mx-auto bg-white rounded-md shadow-sm border-gray-200 border-2 ${className}`}>
-    {title && <div className='border p-2 flex'>
+    {title && <div className={`border p-2 flex ${titleClassName}`}>
       <h1 className='text-xl'>{title}</h1>
     </div>
     }
