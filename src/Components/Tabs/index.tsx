4ff9f010dec989de64dc8b1735cbf7239865d878
Godti Vinod <venkat.godti3@gmail.com>
2022-01-18 21:23:01 +0530

    RcTab bar css import

--- src/Components/Tabs/index.tsx
+++ src/Components/Tabs/index.tsx
@@ -1,6 +1,9 @@
 import { useState, ReactChild, ReactChildren } from 'react';
+import { DotsCircleHorizontalIcon } from '@heroicons/react/solid';
 import RcTabs, { TabPane } from 'rc-tabs';
+import 'rc-tabs/assets/index.css';
 import './tabs.css';
+
 
 interface TabsProps {
   children: ReactChild | ReactChildren
@@ -13,6 +16,8 @@ export const Tabs = ({ children, className }: TabsProps) => {
   const [activeKey, setActiveKey] = useState<string>('1');
   return <RcTabs
     className={`custom-tab-bar ${className}`}
+    direction={'ltr'}
+    moreIcon={<DotsCircleHorizontalIcon className="h-5 hidden" />}
     activeKey={activeKey}
     onChange={key => {
       if (activeKey !== undefined) {
