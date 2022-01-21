ea62020468befbb00ab262f9a48cfaadcb98b521
Godti Vinod <venkat.godti3@gmail.com>
2022-01-21 22:49:24 +0530

    Google login implementation

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,28 +1,17 @@
 import { Card, Wrapper, Typography, Button } from '../../components';
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { auth } from '../../utils/firebase';
+import { useDispatch } from 'react-redux';
+import { LoginRequest } from '../../store/actions';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
-  const googleLogin = () => {
-    const provider = new GoogleAuthProvider();
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        const token = credential?.accessToken;
-        // The signed-in user info.
-        const user = result.user;
-        console.log(user, token)
-      }).catch((error) => {
-        console.log(error)
-      });
-  }
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   return (
     <Card className={`fixed bg-gray-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-fit`}>
       <>
         <Typography variant="h2" className="text-xl text-indigo-500" >Login Here</Typography>
         <Wrapper className="flex flex-row justify-end">
-          <Button onClick={() => googleLogin()} className="m-1" title="Login with google"></Button>
+          <Button onClick={() => dispatch(LoginRequest(navigate))} className="m-1" title="Login with google"></Button>
         </Wrapper>
         <Wrapper className="flex flex-row justify-end">
           <Button onClick={() => { }} className="m-1" title="Login with Github"></Button>
