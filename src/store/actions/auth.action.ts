70e3ec04f9e71964f9d0733cedba7bd39c4d5737
Godti Vinod <venkat.godti3@gmail.com>
2022-01-22 21:40:00 +0530

    Firebase auth implementation for github implementation

--- src/store/actions/auth.action.ts
+++ src/store/actions/auth.action.ts
@@ -1,16 +1,26 @@
 import { LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILED } from '../types';
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { signInWithPopup, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
 import { auth } from '../../utils/firebase';
 import { AppThunk } from '../rootReducer';
+import { Notify, toastTypes } from '../../utils/toast';
 
-const provider = new GoogleAuthProvider();
-export const LoginRequest = (): AppThunk => (dispatch) => {
+const getProviderInstance = (providerType: string) => {
+  if (providerType === 'github') return new GithubAuthProvider();
+  return new GoogleAuthProvider();
+}
+
+const provider = (providerType: string) => {
+  if (providerType === 'github') return GithubAuthProvider;
+  return GoogleAuthProvider;
+}
+
+export const LoginRequest = (providerType: string): AppThunk => (dispatch) => {
   dispatch({
     type: LOGIN_REQUEST
   })
-  signInWithPopup(auth, provider)
+  signInWithPopup(auth, getProviderInstance(providerType))
     .then((result) => {
-      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const credential = provider(providerType).credentialFromResult(result);
       const token = credential?.accessToken;
       if (token) {
         window.localStorage.setItem('firebaseToken', token);
@@ -21,6 +31,13 @@ export const LoginRequest = (): AppThunk => (dispatch) => {
         payload: user
       })
     }).catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      if (errorCode === 'auth/account-exists-with-different-credential') {
+        Notify({ title: 'Account already,Try with other options!', type: toastTypes.ERROR })
+      } else {
+        Notify({ title: errorMessage, type: toastTypes.ERROR })
+      }
       dispatch({
         type: LOGIN_FAILED,
         payload: error
