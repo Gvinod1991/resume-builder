4dd3ff2630313c83a6384b3495bd01fcb5da5719
Godti Vinod <venkat.godti3@gmail.com>
2022-01-29 19:25:52 +0530

    Resume reducer initial state interface refactoring

--- src/store/reducer/resume.reducer.ts
+++ src/store/reducer/resume.reducer.ts
@@ -5,12 +5,54 @@ import {
   GET_RESUME_DATA_REQUEST,
 } from '../types';
 
+type ProfileType = {
+  network: string;
+  url: string;
+};
+
+type SkillType = {
+  skillName?: string;
+  level?: number;
+};
+type WorkType = {
+  companyName: string;
+  position: string;
+  startDate: string;
+  endDate: string;
+  location: string;
+  summary: string;
+};
+type EducationType = {
+  institution: string;
+  studyType: string;
+  fieldOfStudy: string;
+  startDate: string;
+  endDate: string;
+};
+export interface IResumeDetails {
+  location?: string;
+  jobRole?: string;
+  profileImage?: string;
+  email?: string;
+  phone?: string;
+  resumeHighlights?: string;
+  totalExperience?: string;
+  achievements?: string;
+  profiles?: Array<ProfileType>;
+  skills?: {
+    languages: Array<SkillType>;
+    frameworks: Array<SkillType>;
+    libraries: Array<SkillType>;
+    databases: Array<SkillType>;
+    tools: Array<SkillType>;
+  };
+  work?: Array<WorkType>;
+  education?: Array<EducationType>;
+}
+
 export interface IResumeState {
   resumeLoading: Boolean;
-  resumeDetails: {
-    location?: string;
-    jobRole?: string;
-  };
+  resumeDetails: IResumeDetails;
 }
 
 const initialState: IResumeState = {
