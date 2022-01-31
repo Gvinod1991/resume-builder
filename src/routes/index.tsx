6897c268dd2369f8ee50d118c47f40ed48fc2e03
Godti Vinod <venkat.godti3@gmail.com>
2022-01-31 13:05:49 +0530

    Lazy loading with error boundary implementation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,40 @@
+import { Suspense, lazy } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Layout } from '../layout';
 import {
   RouteWithLayout,
   PrivateRouteWithLayout,
 } from '../components/RouteWithLayout';
-import Profile from '../pages/Profile';
-import Resume from '../pages/Resume';
-import Login from '../pages/Login';
+import { Loader } from '../components';
+import { ErrorBoundary } from 'react-error-boundary';
+import { ErrorFallback } from '../components/ErrorFallback';
+const Profile = lazy(() => import('../pages/Profile'));
+const Resume = lazy(() => import('../pages/Resume'));
+const Login = lazy(() => import('../pages/Login'));
 
 export const AppRoutes = (): JSX.Element => {
   return (
     <BrowserRouter>
-      <PrivateRouteWithLayout path='/' Component={Profile} Layout={Layout} />
-      <PrivateRouteWithLayout
-        path='/resume'
-        Component={Resume}
-        Layout={Layout}
-      />
-      <RouteWithLayout path='/login' Component={Login} />
+      <ErrorBoundary
+        FallbackComponent={ErrorFallback}
+        onReset={(): void => {
+          window.location.reload();
+        }}
+      >
+        <Suspense fallback={<Loader />}>
+          <PrivateRouteWithLayout
+            path='/'
+            Component={Profile}
+            Layout={Layout}
+          />
+          <PrivateRouteWithLayout
+            path='/resume'
+            Component={Resume}
+            Layout={Layout}
+          />
+          <RouteWithLayout path='/login' Component={Login} />
+        </Suspense>
+      </ErrorBoundary>
     </BrowserRouter>
   );
 };
