8cd635222ce98e01feb5a6a0e6e2de04fbf5d844
Godti Vinod <venkat.godti3@gmail.com>
2022-02-09 20:43:04 +0530

    Datepicker component slectedValue and onDateChange event implementation

--- src/components/DatePicker/index.tsx
+++ src/components/DatePicker/index.tsx
@@ -1,21 +1,25 @@
-import { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 interface ICustomDatePicker {
   pickerLabel?: string;
+  selectedDate: string;
+  onDateChange: (date: string) => void;
 }
 export const CustomDatePicker = ({
   pickerLabel,
+  selectedDate,
+  onDateChange,
 }: ICustomDatePicker): JSX.Element => {
-  const [startDate, setStartDate] = useState<Date>(new Date());
   return (
     <div className='flex flex-col p-1'>
       <label>{pickerLabel}</label>
       <DatePicker
         className='w-full rounded-md shadow-sm bg-gray-100 focus:border-indigo-500 focus:bg-white focus:ring-indigo-500'
-        selected={startDate}
-        onChange={(date: Date): void => setStartDate(date)}
+        selected={selectedDate ? new Date(selectedDate) : new Date()}
+        onChange={(date: Date): void =>
+          onDateChange(new Date(date).toISOString().slice(0, 10))
+        }
         dateFormat='MM/yyyy'
         showMonthYearPicker
       />
