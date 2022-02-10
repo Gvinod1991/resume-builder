39fea3fee5cf748d3048c13d50b5abba47746592
Godti Vinod <venkat.godti3@gmail.com>
2022-02-10 21:58:15 +0530

    Achievements update functionality implementation

--- src/pages/Profile/Achievements/index.tsx
+++ src/pages/Profile/Achievements/index.tsx
@@ -1,14 +1,34 @@
-import { TextArea, Wrapper, Typography, Button } from '../../../components';
-import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import {
+  TextEditor,
+  Wrapper,
+  Typography,
+  Button,
+  Loader,
+} from '../../../components';
+import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../../../store/rootReducer';
 import { IResumeState } from '../../../store/reducer/resume.reducer';
+import { updateResumeData } from '../../../store/actions';
 
 export const Achievements = (): JSX.Element => {
-  const {
-    resumeDetails: { achievements },
-  }: IResumeState = useSelector((state: RootState) => state.resume);
+  const dispatch = useDispatch();
+  const { resumeDetails, resumeLoading }: IResumeState = useSelector(
+    (state: RootState) => state.resume
+  );
+  const [achievement, setAchievement] = useState<string | undefined>(
+    resumeDetails?.achievements
+  );
+  const handleSubmit = (): void => {
+    const updatedData = {
+      ...resumeDetails,
+      achievements: achievement,
+    };
+    dispatch(updateResumeData(updatedData, resumeDetails?.userId));
+  };
   return (
     <Wrapper className='flex flex-col w-full'>
+      {resumeLoading && <Loader />}
       <Wrapper className='flex flex-row flex-wrap sm:flex-nowrap'>
         <Typography
           variant='h1'
@@ -17,16 +37,18 @@ export const Achievements = (): JSX.Element => {
           Achievements
         </Typography>
         <Wrapper className='flex flex-col ml-2 px-4 w-12/12 sm:w-8/12 '>
-          <TextArea
-            textAreaValue={achievements ? achievements : ''}
-            rows={8}
-            placeholder='Your Achievements'
-            handleChange={(): void => {}}
+          <TextEditor
+            textEditorValue={achievement}
+            handleEditorChange={(content: any): void => setAchievement(content)}
           />
         </Wrapper>
       </Wrapper>
       <Wrapper className='flex justify-end'>
-        <Button title='Save' className='w-fit'></Button>
+        <Button
+          title='Save'
+          onClick={(): void => handleSubmit()}
+          className='w-fit'
+        ></Button>
       </Wrapper>
     </Wrapper>
   );
