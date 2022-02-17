4a98326996885b4c3c530616bea3e31351a6a08a
Godti Vinod <venkat.godti3@gmail.com>
2022-02-17 22:05:38 +0530

    Print css improvement

--- src/utils/print.ts
+++ src/utils/print.ts
@@ -5,6 +5,34 @@ export const printResume = (): void => {
   myWindow?.document.write(
     '<script src="https://cdn.tailwindcss.com"></script>'
   );
+  myWindow?.document.write(
+    `<style> 
+    body{
+      font-size:10px!important;
+    }
+    h1{
+      font-size:14px!important;
+    }
+    h2,h3{
+      font-size:12px!important;
+    }
+    h5,h6{
+      font-size:11px!important;
+    }
+    p{
+      font-size:10px!important;
+    }
+    ul{
+      padding:0.5rem!important;
+      list-style-type:disc!important;
+    }
+    li{
+      margin:0.2rem!important;
+      paddingLeft:1rem!important;
+      font-size:10px!important;
+    }
+  </style>`
+  );
   myWindow?.document.write('</head><body >');
   myWindow?.document.write(data ? data : '');
   myWindow?.document.write('</body></html>');
