6cc472bbea428b8de7c73939a9bebf262d0c8c98
Godti Vinod <venkat.godti3@gmail.com>
2022-03-18 14:24:20 +0530

    Modal compoment title style prop implementation

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -8,8 +8,10 @@ interface IModal {
   open: boolean;
   title: string;
   style?: object;
+  titleStyle?: string;
   className?: string;
   saveBtnTitle?: string;
+  hideCancelBtn?: boolean;
   onClose: () => void;
   onSave?: () => void;
 }
@@ -19,7 +21,9 @@ export const Modal = ({
   children,
   title,
   className,
+  titleStyle,
   saveBtnTitle,
+  hideCancelBtn,
   onClose,
   onSave,
 }: IModal): JSX.Element | null => {
@@ -32,19 +36,24 @@ export const Modal = ({
         scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100 scrollbar-thumb-rounded ${className}`}
       >
         <div className='flex flex-row justify-between'>
-          <Typography variant='h2' className='text-xl'>
+          <Typography variant='h2' className={`text-xl ${titleStyle}`}>
             {title}
           </Typography>
           <XIcon onClick={onClose} className='w-6 h-6 cursor-pointer' />
         </div>
         <div>{children}</div>
+
         <div className='flex flex-row justify-end'>
-          <Button
-            onClick={onSave}
-            className='m-1'
-            title={saveBtnTitle ? saveBtnTitle : 'Ok'}
-          ></Button>
-          <Button onClick={onClose} className='m-1' title='Cancel'></Button>
+          {onSave ? (
+            <Button
+              onClick={onSave}
+              className='m-1'
+              title={saveBtnTitle ? saveBtnTitle : 'Ok'}
+            ></Button>
+          ) : null}
+          {!hideCancelBtn ? (
+            <Button onClick={onClose} className='m-1' title='Cancel'></Button>
+          ) : null}
         </div>
       </div>
     </>,
