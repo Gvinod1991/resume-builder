e03e662fbd3db434e0440d5aaf2ef27a12414e9c
Godti Vinod <venkat.godti3@gmail.com>
2022-03-18 14:24:58 +0530

    Logo update

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,13 +1,17 @@
+import { PencilAltIcon } from '@heroicons/react/solid';
 import React from 'react';
 
 export const Logo = (): JSX.Element => {
   return (
     <div className='flex-shrink-0 flex items-center p-4'>
       <h1 className='text-gray-50 rounded-md text-lg font-medium font-mono hidden lg:block'>
-        Resume Builder
+        <PencilAltIcon className='w-8 cursor-pointer inline' />
+        <span className='relative top-1 font-bold text-lg uppercase'>
+          ResumeSmith
+        </span>
       </h1>
       <h1 className='text-gray-50 rounded-md text-lg font-medium font-mono block lg:hidden'>
-        R
+        <PencilAltIcon className='w-8 cursor-pointer inline' />
       </h1>
     </div>
   );
